17601d4526aae0237d6c15a8c8ad8828022701d2
Léo <legrivel@student.le-101.fr>
2019-05-14 17:31:40 +0200

    Getting each section name

--- main.c
+++ main.c
@@ -29,6 +29,22 @@ static int  checkFileType(unsigned char mnum[EI_NIDENT]) {
     return (-1);
 }
 
+static void *getSection(Elf64_Ehdr *header, const char *section) {
+    Elf64_Half  i;
+    Elf64_Shdr  *strTable;
+    Elf64_Shdr  *secHeader;
+
+    secHeader = ((void *)header) + header->e_shoff;
+    strTable = secHeader + header->e_shstrndx;
+    i = 0;
+    while (i < header->e_shnum) {
+        dprintf(1, "Section:  %s\n", (char *)((void *)header + strTable->sh_offset + (secHeader + i)->sh_name));
+        i += 1;
+    }
+    (void)section;
+    return (NULL);
+}
+
 int main(int argc, char **argv) {
     int         fd;
     Elf64_Ehdr  *header;
@@ -47,6 +63,7 @@ int main(int argc, char **argv) {
     close(fd);
     if (checkFileType(header->e_ident) == -1)
         return (1);
+    getSection(header, "HelloWorld");
     return (0);
 }
 
