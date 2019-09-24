44c60e30dfa3222c2a3de4c1b5a5435e1bb11432
Grivel-l <legrivel@student.le-101.fr>
2019-09-24 21:08:44 +0200

    HelloWorld shellcode

--- srcs/loader.s
+++ srcs/loader.s
@@ -1,3 +1,25 @@
 [bits 64]
-nop
-int3
+
+section .text
+  nop
+  nop
+  nop
+  nop
+  call put
+  call exit
+
+  msg:
+    db "HelloWorld", 10
+  put:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 11
+    syscall
+    ret
+
+  exit:
+    int3
+    mov rax, 60
+    mov rdi, 0
+    syscall
